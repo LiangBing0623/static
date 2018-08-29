727241989bc4585d9b6beaf924e19d756af87e4f
LiangBing <425620857@qq.com>
2018-08-29 16:27:37 +0800

    提提交

---
+++ static/js/config.js
@@ -0,0 +1,12 @@
+model_config={
+	url:'http://yh.supe-paint.com/api/h5',
+	// url:'http://103.40.4.11:8098/api/h5',
+	// key:'c4ca4238a0b923820dcc509a6f75849b',
+	
+	// key:"29be4acb058a8fa9c4733003f538bc7d",
+	// token:"f49a1674ff9742af904dcf05c1d869a2",
+	
+	token:'default',
+	key:'7f4de0852b764d26bc6137f92c1e5c2c',
+	iv:'1234567890!@#$%^'
+}
---
+++ static/lang/en_US.js
@@ -0,0 +1,186 @@
+// 英文
+module.exports={
+	// tab 标签
+	"下注":"bet",
+	"开奖":"result",
+	"我的":"me",
+
+	//首页
+	"自选":"select",
+	"机选":"lucky pick",
+	"打单":"box",
+	"球一":"ball 1",
+	"球二":"ball 2",
+	"球三":"ball 3",
+	"球四":"ball 4",
+	"彩票类型":"types",
+	"全选":"all",
+	"期数":"date",
+	"块角分":"money",
+	"玩法":"betting",
+	"大":"big",
+	"小":"small",
+	"单":"",
+	"种类":"types",
+	"单程":"single",
+	"来回":"return",
+	"全打":"box",
+	"发送":"send",
+	"清空":"empty",
+	"下注确认":"confirm",
+	"号码":"number",
+	"彩种":"option",
+	"金额":"credit",
+	"确认":"confirm",
+	"取消":"cancel",
+	"幸运号码":"lucky number",
+	"块":"$1",
+	"角":"￠10",
+	"分":"￠1",
+	"代理":"agent",
+	"最多可选4个号码":"up to 4 number",
+	"金额必须为数字":"amount must be a number",
+	"号码必须为数字":"fill number",
+	"选择号码个数错误":"num.error",
+	"未选择号码":"pls choice Number",
+	"未选择期数":"pls select date",
+	"未输入玩法":"pls fill types",
+	"玩法金额不能为零":"pls fill in your betting",
+	"未选择彩票类型":"pls select your types",
+	"未选择玩法":"pls select betting",
+	"是否重复下注":"repeat or no",
+	"您还未选择球":"the ball has not been selected",
+	"一":"1",
+	"二":"2",
+	"三":"3",
+	"四":"4",
+	"重置":"",
+
+	// 打单
+	"输入不完整":"incomplete",
+	"还未选择日期":"pls select date",
+	"输入错误":"error",
+	"重复输入":"re enter",
+	"号码输入错误":"number input error",
+	"区间选择错误":"interval selection error",
+	"未找到会员":"",
+
+	// 开奖日期
+	"开奖日期":"draw date",
+	"查询":"search",
+	"一等奖":"1st",
+	"二等奖":"2nd",
+	"三等奖":"3rd",
+	"特别奖":"special",
+	"安慰奖":"consolation",
+	"选择日期非开奖日期":"selected date is`t the date of the draw",
+	"万能":"magnum",
+    "四合彩":"1+3",
+    "多多":"toto",
+    "新加坡":"singapore",
+    "沙巴":"sabah(88)",
+    "山打根":"stc",
+    "砂拉越":"sarawak(cash sweep)",
+
+    // "公告信息"
+    "公告信息":"information",
+    //配套信息
+    "配套信息":"packege list",
+    "佣金":"commission",
+    "奖金":"bonus",
+
+	// 财务明细
+    "财务明细":"finance detial",
+    "开始日期":"start date",
+    "截止日期":"deadline",
+    "调整前":"before",
+    "调整积分":"adjust",
+    "上下分":"top up",
+    "调整后":"after",
+    "操作时间":"operating time",
+    "选择错误":"select error",
+    "至":"to",
+
+    // 注单明细
+    "注单明细":"receipt detial",
+	"全部":"all",
+	"订单号":"receipt",
+	"投注时间":"bet time",
+	"开奖公司":"draw company",
+	"下注金额":"total bet",
+	"重买":"repeat",
+	// 注单历史
+	"注单历史":"bet detail",
+	"盈亏":"win/lose",
+
+	// 修改密码
+	"修改密码":"change/pass",
+	"旧密码":"old/pass",
+	"请输入旧密码":"enter old /pass",
+	"新密码":"new password",
+	"请输入旧密码":"",
+	"确认新密码":"",
+   	"请输入新密码":"",
+	"保存修改":"",
+	"两次输入不一致":"",
+	"请确认新密码":"",
+	"修改成功":"confirm",
+	
+	// 登录界面
+	'登录':'login',
+	"请输入用户名":"Please enter the username",
+	'请输入验证码':'Please enter the verification code',
+	"请输入密码":"password",
+
+	// 我的
+	"会员":"member",
+	"所属代理":"agent",
+	"余分":"balance",
+	"公告信息":"messege",
+	"配套信息":"package list",
+	"财务明细":"finance detail",
+	"注单明细":"bet detail",
+	"注单历史":"bet story",
+	"多期注单":"multi receipt",
+	"修改密码":"change/pass",
+	"退出登录":"log out",
+	"确定退出登录？":"confirm log out",
+
+
+	"没有更多了":"no more",
+	"正在加载...":"loading",
+	"中奖记录":"中奖记录",
+	"总额":""
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
